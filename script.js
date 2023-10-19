c784177e267879c5599bc40a8b985fbaadab9274
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-19 11:30:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const fname=document.getElementById("fname");
+	const lname=document.getElementById("lname");
+	alert(fname.textContent+" "+lname.textContent);
 }
